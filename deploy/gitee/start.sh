18b11fac9a93dee12b94e2767d3c5d38a9673998
luguangdong <lgd15095370993@gmail.com>
2023-11-08 18:25:28 +0800

    init gateway.

--- deploy/gitee/start.sh
+++ deploy/gitee/start.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+source /etc/profile
 nohup java  -Xms128m -Xmx128m -Xmn128m -Dproject.name=gateway -jar /data/deploy/lucifer-cloud/gateway/gateway.jar  --spring.profiles.active=tencent > log &
