81322e09386adca217ec62c6292aba21298597ba
luguangdong <lgd15095370993@gmail.com>
2023-12-06 13:16:38 +0800

    optimize.

--- deploy/gitee/start.sh
+++ deploy/gitee/start.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
+FILEPATH=/data/deploy/lucifer-cloud/gateway
+JARFILE=gateway.jar
+ENV=tencent
+pid=`ps -ef | grep $JARFILE | grep -v grep | awk '{print $2}'`
+echo "旧服务进程pid=$pid"
+if [ -n "$pid" ]
+then
+	kill -9 $pid
+	echo "关闭服务 $JARFILE 成功"
+fi
 source /etc/profile
-nohup java  -Xms128m -Xmx128m -Xmn128m -Dproject.name=gateway -jar /data/deploy/lucifer-cloud/gateway/gateway.jar  --spring.profiles.active=tencent > log &
+nohup java  -Xms128m -Xmx128m -Xmn128m -jar $FILEPATH/$JARFILE  --spring.profiles.active=$ENV > log &
+pid=`ps -ef | grep $JARFILE | grep -v grep | awk '{print $2}'`
+echo "新服务进程pid=$pid"
+echo "启动服务 $JARFILE 成功"
