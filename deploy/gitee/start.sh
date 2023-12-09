056f7b1ffefca3dd4986ac9ee160a7067218cb50
luguangdong <lgd15095370993@gmail.com>
2023-12-09 16:17:46 +0800

    optimize.

--- deploy/gitee/start.sh
+++ deploy/gitee/start.sh
@@ -14,3 +14,4 @@ nohup java  -Xms128m -Xmx128m -Xmn128m -jar $FILEPATH/$JARFILE  --spring.profile
 pid=`ps -ef | grep $JARFILE | grep -v grep | awk '{print $2}'`
 echo "新服务进程pid=$pid"
 echo "启动服务 $JARFILE 成功"
+set -e
